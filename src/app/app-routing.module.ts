cca664a80c9b9248ec8f7a76d614c47af24e293f
Roberto Orcasita <robertorcasitta@gmail.com>
2021-09-07 21:55:54 -0500

    Falta Division

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import{HomeComponent}from'../app/components/home/home.component';
+import {SumaComponent} from '../app/components/suma/suma.component';
+import {RestaComponent} from '../app/components/resta/resta.component';
+import { MultiplicacionComponent } from './components/multiplicacion/multiplicacion.component';
+import { DivisionComponent } from './components/division/division.component';
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'suma',component:SumaComponent},
+  {path:'resta',component:RestaComponent},
+  {path:'multiplicacion',component:MultiplicacionComponent},
+  {path:'division',component:DivisionComponent},
+  { path: '**', pathMatch: 'full',redirectTo:'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SumaComponent } from './components/suma/suma.component';
+import { RestaComponent } from './components/resta/resta.component';
+import { MultiplicacionComponent } from './components/multiplicacion/multiplicacion.component';
+import { DivisionComponent } from './components/division/division.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SumaComponent,
+    RestaComponent,
+    MultiplicacionComponent,
+    DivisionComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
